be77abff7c97f89686f1b348c1a0fcf0c093e615
al121 <kb_299@bk.ru>
2023-05-21 08:41:36 +0300

    Initial commit

---
+++ src/main/java/ru/gb/homework7spring/HomeWork7SpringApplication.java
@@ -0,0 +1,14 @@
+package ru.gb.homework7spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork7SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork7SpringApplication.class, args);
+
+    }
+
+}
